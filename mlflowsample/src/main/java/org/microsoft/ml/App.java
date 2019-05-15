8c99454238148498a5cb32ca5a8dd219071e39db
Akshaya Annavajhala (AK) <akannava@microsoft.com>
2019-05-15 03:03:27 -0400

    use ADAL directly

--- mlflowsample/src/main/java/org/microsoft/ml/App.java
+++ mlflowsample/src/main/java/org/microsoft/ml/App.java
@@ -1,5 +1,13 @@
 package org.microsoft.ml;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import com.microsoft.aad.adal4j.AuthenticationContext;
+import com.microsoft.aad.adal4j.AuthenticationResult;
+import com.microsoft.aad.adal4j.ClientCredential;
+
 import org.mlflow.api.proto.Service.RunInfo;
 import org.mlflow.tracking.MlflowClient;
 import org.mlflow.tracking.creds.BasicMlflowHostCreds;
@@ -10,11 +18,21 @@ import org.mlflow.tracking.creds.MlflowHostCredsProvider;
  *
  */
 public class App {
+
+    // https://github.com/Azure-Samples/active-directory-java-native-headless/blob/master/src/main/java/PublicClient.java
+    private final static String AUTHORITY = "https://login.microsoftonline.com/common/";
+    private final static String TARGET_RESOURCE = "https://management.core.windows.net/";
+
+    // https://docs.microsoft.com/en-us/cli/azure/create-an-azure-service-principal-azure-cli?view=azure-cli-latest#password-based-authentication
+    private final static String CLIENT_ID = "<your_SP_client_id>";
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello AzureML!");
 
-        String trackingUri = "https://eastus2.experiments.azureml.net//history/v1.0/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.MachineLearningServices/workspaces/{workspaceName}";
-        String armToken = System.getenv("MLFLOW_TRACKING_TOKEN");
+        AuthenticationResult result = getAccessTokenFromUserCredentials();
+
+        String trackingUri = "https://eastus2.experiments.azureml.net/history/v1.0/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.MachineLearningServices/workspaces/{workspaceName}";
+        String armToken = result.getAccessToken();
         if (armToken == null) {
             throw new Exception("Provide a valid ARM token.");
         }
@@ -28,5 +46,25 @@ public class App {
         client.logMetric(runInfo.getRunUuid(), "accuracy", 0.9);
 
         System.out.println("Goodbye AzureML!");
+    }
+
+    private static AuthenticationResult getAccessTokenFromUserCredentials() throws Exception {
+        AuthenticationContext context;
+        AuthenticationResult result;
+        ExecutorService service = null;
+        try {
+            service = Executors.newFixedThreadPool(1);
+            context = new AuthenticationContext(AUTHORITY, false, service);
+            Future<AuthenticationResult> future = context.acquireToken(TARGET_RESOURCE,
+                    new ClientCredential(CLIENT_ID, System.getenv("AZUREMLFLOW_SP_PASSWORD")), null);
+            result = future.get();
+        } finally {
+            service.shutdown();
+        }
+
+        if (result == null) {
+            throw new Exception("authentication result was null");
+        }
+        return result;
     }
 }
